e587e4a552a0b3b05a81a17c01ad4be4722e65bb
carloslavie <carlosmaria.lavie@nttdata.com>
2022-11-09 17:56:16 -0300

    section list component

--- src/screens/PullToRefreshScreen.tsx
+++ src/screens/PullToRefreshScreen.tsx
@@ -5,21 +5,30 @@ import {styles} from '../theme/appTheme';
 
 export const PullToRefreshScreen = () => {
   const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState<string>();
 
   const onRefresh = () => {
     setRefreshing(true);
     setTimeout(() => {
       console.log('terminamos');
       setRefreshing(false);
+      setData('Hola mundo');
     }, 1500);
   };
   return (
     <ScrollView
       refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          progressViewOffset={10}
+          progressBackgroundColor="#5856D6"
+          colors={['white', 'red', 'orange']}
+        />
       }>
       <View style={styles.globalMargin}>
         <HeaderTitle title="Pull to refresh" />
+        {data && <HeaderTitle title={data} />}
       </View>
     </ScrollView>
   );
